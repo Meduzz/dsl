852c03e9ad5575e0dc42361c9afcb887f672b2f1
Meduzz <drgevalia@gmail.com>
2024-08-18 08:31:13 +0200

    Lets call it polish.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Service definition DSL. The DSL focuses on describing either what the service pr
 Most functions are passthrough so you can use the NewX in the Add|SetX method and assign it to a variable and work with it. Ie:
 
 ```go
-myEndpoint := myService.AddEndpoint(GET("/my/path"))
+myEndpoint := myService.AddEndpoint(GET("/my/path")) // GET is the NewX and AddEndpoint is passthrough,
 myEndpoint.Name = "myEndpoint"
 // ...
 ```
--- app/app_test.go
+++ app/app_test.go
@@ -20,8 +20,51 @@ func TestApp(t *testing.T) {
 	root.Returns.SetType("")
 	s1pa1 := s1.AddParam(service.Env("DB_URL"))
 	s1pa1.Description = "The DSN to connect to the DB."
+	p := app.SetPolicy()
+
+	// define our relations
+	owns := p.Relationship("owner")
+	edits := p.Relationship("edit")
+	views := p.Relationship("view")
+	parents := p.Relationship("parent")
+
+	// define our namespaces
+	document := p.Namespace("document")
+	folder := p.Namespace("folder")
+	user := p.Namespace("user")
+
+	// user relations
+	documentViewer := p.Relation(views, user.Subject(), document.Subject())
+	documentEditor := p.Relation(edits, user.Subject(), document.Subject())
+	documentEditor.Inherit(documentViewer)
+	documentOwner := p.Relation(owns, user.Subject(), document.Subject())
+	documentOwner.Inherit(documentEditor, documentViewer)
+
+	folderViewer := p.Relation(views, user.Subject(), document.Subject())
+	folderEditor := p.Relation(edits, user.Subject(), folder.Subject())
+	folderEditor.Inherit(folderViewer)
+	folderOwner := p.Relation(owns, user.Subject(), folder.Subject())
+	folderOwner.Inherit(folderEditor, folderViewer)
+
+	// define folder relations
+	folderParent := p.Relation(parents, folder.Subject(), folder.Subject())
+	folderParent.Inherit(folderOwner, folderEditor, folderViewer)
+
+	folderDocumentViewer := p.Relation(views, folder.Subject(), document.Subject())
+	folderDocumentEditor := p.Relation(edits, folder.Subject(), document.Subject())
+	folderDocumentEditor.Inherit(folderDocumentViewer)
+	folderDocumentOwner := p.Relation(owns, folder.Subject(), document.Subject())
+	folderDocumentOwner.Inherit(folderDocumentEditor, folderDocumentViewer)
 
 	bs, _ := json.Marshal(app)
 
 	println(string(bs))
+
+	t.Run("figure out stuff", func(t *testing.T) {
+		t.Run("as a document owner, what can I else do?", func(t *testing.T) {
+			for _, it := range documentOwner.Inherits {
+				println("I can also", it.Relation)
+			}
+		})
+	})
 }
