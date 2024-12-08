7a0a6c908dc455a58a3a80e9dd90645faaf9f2b7
Meduzz <drgevalia@gmail.com>
2024-12-08 19:16:33 +0100

    Add quickapi helper to api builder

---
+++ api/qapi/quickapi.go
@@ -0,0 +1,59 @@
+package qapi
+
+import (
+	"fmt"
+
+	"github.com/Meduzz/dsl/api"
+)
+
+const cType = "application/json"
+
+func Quickapi(api *api.Api, prefix, name string, entity any) {
+	create := api.POST(fmt.Sprintf("/%s/%s/", prefix, name))
+	create.Description = fmt.Sprintf("Create a new %s entity", name)
+	createReq := create.BodyVariable("body", cType)
+	createReq.SetType(entity)
+	createResp := create.SetResponse(cType)
+	createResp.SetType(entity)
+
+	read := api.GET(fmt.Sprintf("/%s/%s/:id", prefix, name))
+	read.Description = fmt.Sprintf("Read an %s entity by id", name)
+	read.PathVariable("id")
+	readResp := read.SetResponse(cType)
+	readResp.SetType(entity)
+
+	update := api.PUT(fmt.Sprintf("/%s/%s/:id", prefix, name))
+	update.Description = fmt.Sprintf("Update an %s entity by id", name)
+	updateReq := update.BodyVariable("body", cType)
+	updateReq.SetType(entity)
+	updateResp := update.SetResponse(cType)
+	updateResp.SetType(entity)
+
+	remove := api.DELETE(fmt.Sprintf("/%s/%s/:id", prefix, name))
+	remove.Description = fmt.Sprintf("Delete an %s entity by id", name)
+	remove.PathVariable("id")
+
+	search := api.GET(fmt.Sprintf("/%s/%s/", prefix, name))
+	search.Description = fmt.Sprintf("Read an %s entity by id", name)
+	sSkip := search.QueryVariable("skip")
+	sSkip.SetType(0)
+	sTake := search.QueryVariable("take")
+	sTake.SetType(25)
+	sWhere := search.QueryVariable("where")
+	sWhere.MapOf("string")
+	sSort := search.QueryVariable("sort")
+	sSort.MapOf("string")
+	sPreload := search.QueryVariable("preload")
+	sPreload.MapOf("string")
+	searchResp := read.SetResponse(cType)
+	searchResp.ArrayOf(entity)
+
+	patch := api.PATCH(fmt.Sprintf("/%s/%s/:id", prefix, name))
+	patch.Description = fmt.Sprintf("Patch individual fields of an %s entity", name)
+	patch.PathVariable("id")
+	patchReq := patch.BodyVariable("body", cType)
+	patchReq.Map = true
+	patchReq.Type = "any"
+	patchResp := patch.SetResponse(cType)
+	patchResp.SetType(entity)
+}
